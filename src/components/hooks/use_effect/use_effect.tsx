f7dfad62b81fbb2d2a13d412fae969b669968c63
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-08 16:02:27 +0000

    feat: Implements useEffect and calls and endpoint and displays the data as a string

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,12 +1,21 @@
-// make an api call using useEffect
+import { useState, useEffect } from "react";
+
+const endpoint = "https://jsonplaceholder.typicode.com/todos/1";
 
 export const APICall = () => {
-  // Your code here!
+  const [json, setJson] = useState<string>("");
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((data) => setJson(JSON.stringify(data)))
+      .catch((error) => console.error("Error", error));
+  }, []);
 
   return (
     <>
       <h2>useEffect</h2>
-      <p>Your todo here!</p>
+      <p>{json}</p>
     </>
   );
 };
